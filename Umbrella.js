a80b96de6399796cfc706e1243a96d002dde14f4
Typical-dev <70384457+Typical-dev@users.noreply.github.com>
2020-11-01 17:01:04 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,24 @@
+class Umbrella {
+    constructor(x, y) {
+      var options = {
+          isStatic : true
+      }
+      this.body = Matter.Bodies.circle(x, y, 30, options);
+      this.x = x;
+      this.y = y;
+
+      World.add(world, this.body);
+    }
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      fill(255);
+      ellipseMode(RADIUS);
+      ellipse(0, 0, this.radius, this.radius);
+      pop();
+    }
+  }
+  
